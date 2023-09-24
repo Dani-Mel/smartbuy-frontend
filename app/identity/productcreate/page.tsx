fc00661ace8670b2e31a9de93d8b14c0aeb763f2
Dani-Mel <fildanisimo@gmail.com>
2023-09-24 07:35:43 -0700

    Create product

--- app/identity/productcreate/page.tsx
+++ app/identity/productcreate/page.tsx
@@ -7,37 +7,11 @@ import styles from "./productcreate.module.css";
 import React, { useState } from 'react';
         
         const errorField = useRef<HTMLSpanElement>(null);
-        const productNameInput = document.getElementById('productName') as HTMLInputElement;
-        const productPriceInput = document.getElementById('productPrice') as HTMLInputElement;
-        const messageDiv = document.getElementById('message');
-    
-        const productName = productNameInput.value;
-        const productPrice = parseFloat(productPriceInput.value);
-
-        const product = {
-            name: productName,
-            price: productPrice,
-        };
-        const fileInput = document.createElement('input');
-fileInput.type = 'file';
-fileInput.accept = 'image/*'; // Allow only image files
-
-// Attach the file upload event handler
-fileInput.addEventListener('change', (event) => {
-    const selectedFile = (event.target as HTMLInputElement).files?.[0];
-
-    if (selectedFile) {
-        console.log(`Selected File: ${selectedFile.name}`);
-        // Handle the selected file as needed (e.g., upload to a server)
-    }
-});
-function YourComponent() {
   const [formData, setFormData] = useState({
     name: '',
     description: '',
     file: null, // Initialize a state variable for the file input
   });
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
@@ -48,13 +22,6 @@ function YourComponent() {
     setFormData({ ...formData, file });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Here, you can access formData.name, formData.description, and formData.file
-    // Perform your submit logic here, e.g., call an API to create a product
-  };
-// Add the file input to the body of the document
-document.body.appendChild(fileInput);
         return (
             <>
     <div className={styles.center}>
